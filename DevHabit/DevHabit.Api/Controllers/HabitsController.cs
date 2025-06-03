88dbe6266c3199207d6d886683438403b4da178c
twilight <twilight@deeplay.io>
2025-06-03 13:44:50 +0300

    Старт проекта
    Добавление сущностей, контроллеров и первой миграции

---
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -0,0 +1,102 @@
+﻿using DevHabit.Api.Database;
+using DevHabit.Api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Controllers;
+
+[ApiController]
+[Route("habits")]
+public sealed class HabitsController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<HabitsCollectionDto>> GetHabits()
+    {
+        List<HabitDto> habits = await dbContext
+            .Habits
+            .Select(h => new HabitDto
+            {
+                Id = h.Id,
+                Name = h.Name,
+                Description = h.Description,
+                Type = h.Type,
+                Frequency = new FrequencyDto
+                {
+                    Type = h.Frequency.Type,
+                    TimesPerPeriod = h.Frequency.TimesPerPeriod
+                },
+                Target = new TargetDto
+                {
+                    Unit = h.Target.Unit,
+                    Value = h.Target.Value
+                },
+                Status = h.Status,
+                IsArchived = h.IsArchived,
+                EndDate = h.EndDate,
+                Milestone = h.Milestone == null
+                    ? null
+                    : new MilestoneDto
+                    {
+                        Target = h.Milestone.Target,
+                        Current = h.Milestone.Current
+                    },
+                CreatedAtUtc = h.CreatedAtUtc,
+                UpdatedAtUtc = h.UpdatedAtUtc,
+                LastCompletedAtUtc = h.LastCompletedAtUtc
+            })
+            .ToListAsync();
+
+        ActionResult<HabitsCollectionDto> result = Ok(new HabitsCollectionDto
+        {
+            Data = habits
+        });
+
+        return result;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<HabitDto>> GetHabit(string id)
+    {
+         HabitDto habit = await dbContext
+            .Habits
+            .Where(h => h.Id == id)
+            .Select(h => new HabitDto
+            {
+                Id = h.Id,
+                Name = h.Name,
+                Description = h.Description,
+                Type = h.Type,
+                Frequency = new FrequencyDto
+                {
+                    Type = h.Frequency.Type,
+                    TimesPerPeriod = h.Frequency.TimesPerPeriod
+                },
+                Target = new TargetDto
+                {
+                    Unit = h.Target.Unit,
+                    Value = h.Target.Value
+                },
+                Status = h.Status,
+                IsArchived = h.IsArchived,
+                EndDate = h.EndDate,
+                Milestone = h.Milestone == null
+                    ? null
+                    : new MilestoneDto
+                    {
+                        Target = h.Milestone.Target,
+                        Current = h.Milestone.Current
+                    },
+                CreatedAtUtc = h.CreatedAtUtc,
+                UpdatedAtUtc = h.UpdatedAtUtc,
+                LastCompletedAtUtc = h.LastCompletedAtUtc
+            })
+            .FirstOrDefaultAsync();
+
+         if (habit is null)
+         {
+             return NotFound();
+         }
+
+         return Ok(habit);
+    }
+}
---
+++ DevHabit/DevHabit.Api/Database/SchemaNames.cs
@@ -0,0 +1,6 @@
+﻿namespace DevHabit.Api.Database;
+
+public static class SchemaNames
+{
+    public const string DevHabit = "dev_habit";
+}
